87f82d22abf93d7c5f450e7ee532985bb8944fb5
Jean <jeanmatheus0612@gmail.com>
2025-04-23 20:45:31 -0300

    Adicionando os sintomas nas classes

--- src/main/java/br/com/jean/diagnostico/model/DiagnosticoCovid.java
+++ src/main/java/br/com/jean/diagnostico/model/DiagnosticoCovid.java
@@ -2,13 +2,27 @@ package br.com.jean.diagnostico.model;
 
 import br.com.jean.diagnostico.base.Diagnostico;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class DiagnosticoCovid extends Diagnostico {
-    public DiagnosticoCovid(String[] sintomasComuns) {
-        super(sintomasComuns);
+
+    public DiagnosticoCovid() {
+        super(new String[]{"febre", "tosse seca", "perda de olfato", "dificuldade para respirar"});
     }
 
     @Override
     public String avaliarPaciente(String[] sintomasInformados) {
-        return "";
+        List<String> sintomasList = Arrays.asList(sintomasComuns);
+        int contador = 0;
+
+        for (String sintoma : sintomasInformados) {
+            if (sintomasList.contains(sintoma.toLowerCase())) {
+                contador++;
+            }
+        }
+
+        return (contador >= 2) ? "Provável Covid. Procure orientação médica e mantenha isolamento."
+                : "Poucos sintomas compatíveis com Covid.";
     }
 }
--- src/main/java/br/com/jean/diagnostico/model/DiagnosticoGripe.java
+++ src/main/java/br/com/jean/diagnostico/model/DiagnosticoGripe.java
@@ -2,13 +2,23 @@ package br.com.jean.diagnostico.model;
 
 import br.com.jean.diagnostico.base.Diagnostico;
 
+import java.util.Arrays;
+
 public class DiagnosticoGripe extends Diagnostico {
-    public DiagnosticoGripe(String[] sintomasComuns) {
-        super(sintomasComuns);
+
+    public DiagnosticoGripe() {
+        super(new String[]{"febre", "tosse", "dor de cabeça", "cansaço"});
     }
 
     @Override
     public String avaliarPaciente(String[] sintomasInformados) {
-        return "";
+        int contador = 0;
+        for (String sintoma : sintomasInformados) {
+            if (Arrays.asList(sintomasComuns).contains(sintoma)) {
+                contador++;
+            }
+        }
+        return (contador >= 2) ? "Provável Gripe. Repouso e hidratação recomendados."
+                : "Poucos sintomas compatíveis com Gripe.";
     }
 }
